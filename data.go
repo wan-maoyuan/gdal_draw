5b3a8b32288360a0ea6c7444efbf7bf557b6991c
wan-maoyuan <wanmaoyuan@outlook.com>
2024-12-09 14:29:57 +0800

    update

--- data.go
+++ data.go
@@ -1,16 +1,18 @@
 package gdaldraw
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Data struct {
-	LatList     []float64 // 纬度需要从 -90~90
-	LonList     []float64 // 经度需要从 -180~180
+	LatList     []float64 // 纬度需要从 北纬90 到 南纬90
+	LonList     []float64 // 经度需要从 西经180 到 东经180
 	Accuracy    float64   // 地图的精度
 	ValueList   [][]float64
 	OutFilePath string
 }
 
-func checkData(data *Data) error {
+func (data *Data) check() error {
 	if len(data.LatList) == 0 {
 		return fmt.Errorf("lat slice is empty")
 	}
@@ -29,6 +31,95 @@ func checkData(data *Data) error {
 
 	if len(data.LonList) != len(data.ValueList[0]) {
 		return fmt.Errorf("value second slice length not equal lon slice length")
+	}
+
+	if data.Accuracy <= 0 {
+		return fmt.Errorf("lat lon accuracy is invalid")
+	}
+
+	if data.OutFilePath == "" {
+		return fmt.Errorf("out file path is empty")
+	}
+
+	return nil
+}
+
+type DoubleData struct {
+	LatList     []float64 // 纬度需要从 北纬90 到 南纬90
+	LonList     []float64 // 经度需要从 西经180 到 东经180
+	Accuracy    float64   // 地图的精度
+	Value1List  [][]float64
+	Value2List  [][]float64
+	OutFilePath string
+}
+
+func (data *DoubleData) check() error {
+	if len(data.LatList) == 0 {
+		return fmt.Errorf("lat slice is empty")
+	}
+
+	if len(data.LonList) == 0 {
+		return fmt.Errorf("lon slice is empty")
+	}
+
+	if len(data.Value1List) == 0 {
+		return fmt.Errorf("value1 slice is empty")
+	}
+
+	if len(data.Value2List) == 0 {
+		return fmt.Errorf("value2 slice is empty")
+	}
+
+	if len(data.LatList) != len(data.Value1List) {
+		return fmt.Errorf("value1 first slice length not equal lat slice length")
+	}
+
+	if len(data.LatList) != len(data.Value2List) {
+		return fmt.Errorf("value2 first slice length not equal lat slice length")
+	}
+
+	if len(data.LonList) != len(data.Value1List[0]) {
+		return fmt.Errorf("value1 second slice length not equal lon slice length")
+	}
+
+	if len(data.LonList) != len(data.Value2List[0]) {
+		return fmt.Errorf("value2 second slice length not equal lon slice length")
+	}
+
+	if data.Accuracy <= 0 {
+		return fmt.Errorf("lat lon accuracy is invalid")
+	}
+
+	if data.OutFilePath == "" {
+		return fmt.Errorf("out file path is empty")
+	}
+
+	return nil
+}
+
+type IrregularData struct {
+	LatList     []float64 // 纬度范围： 90 到 -90
+	LonList     []float64 // 经度范围：-180 到 180
+	ValueList   []float64
+	Accuracy    float64 // 地图的精度
+	OutFilePath string
+}
+
+func (data *IrregularData) check() error {
+	if len(data.LatList) == 0 {
+		return fmt.Errorf("lat slice is empty")
+	}
+
+	if len(data.LonList) == 0 {
+		return fmt.Errorf("lon slice is empty")
+	}
+
+	if len(data.ValueList) == 0 {
+		return fmt.Errorf("value slice is empty")
+	}
+
+	if len(data.LatList) != len(data.ValueList) {
+		return fmt.Errorf("value slice length not equal lat slice length")
 	}
 
 	if data.Accuracy <= 0 {
--- draw.go
+++ draw.go
@@ -14,29 +14,31 @@ import (
 const (
 	MinValue        = -20037508.34
 	MaxValue        = 20037508.34
-	PictureAccuracy = 2048 // 图片精度
+	PictureAccuracy = 2049 // 图片精度
 	StepValue       = (MaxValue - MinValue) / PictureAccuracy
 )
 
 type ColorFunc func(img *image.RGBA, x, y int, value float64)
+
+type DounbleColorFunc func(img *image.RGBA, x, y int, value1, value2 float64)
 
 // 默认着色函数填充红色，这个函数可以自定义
 func DefaultColorFunc(img *image.RGBA, x, y int, value float64) {
 	var colorValue = uint8(value)
 
 	img.SetRGBA(x, y, color.RGBA{
-		colorValue, 0, 0, colorValue,
+		colorValue, 0, 0, 255,
 	})
 }
 
 func Draw3857(data *Data, colorFunc ColorFunc) error {
-	if err := checkData(data); err != nil {
+	if err := data.check(); err != nil {
 		return err
 	}
 
-	img := image.NewRGBA(image.Rect(0, 0, 2048, 2048))
-	for xIndex := 0; xIndex < 2048; xIndex++ {
-		for yIndex := 0; yIndex < 2048; yIndex++ {
+	img := image.NewRGBA(image.Rect(0, 0, 2049, 2049))
+	for xIndex := 0; xIndex <= 2048; xIndex++ {
+		for yIndex := 0; yIndex <= 2048; yIndex++ {
 			x := MinValue + StepValue*float64(xIndex)
 			y := MinValue + StepValue*float64(yIndex)
 
@@ -67,9 +69,93 @@ func Draw3857(data *Data, colorFunc ColorFunc) error {
 	return nil
 }
 
+func DrawDouble3857(data *DoubleData, colorFunc DounbleColorFunc) error {
+	if err := data.check(); err != nil {
+		return err
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, 2049, 2049))
+	for xIndex := 0; xIndex <= 2048; xIndex++ {
+		for yIndex := 0; yIndex <= 2048; yIndex++ {
+			x := MinValue + StepValue*float64(xIndex)
+			y := MinValue + StepValue*float64(yIndex)
+
+			lat, lon := convert3857To4326(x, y)
+			latIndex := int((lat + 90) / data.Accuracy)
+			lonIndex := int((lon + 180) / data.Accuracy)
+
+			value1 := data.Value1List[latIndex][lonIndex]
+			value2 := data.Value2List[latIndex][lonIndex]
+
+			colorFunc(img, xIndex, yIndex, value1, value2)
+		}
+	}
+
+	file, err := os.Create(data.OutFilePath)
+	if err != nil {
+		return fmt.Errorf("create out file: %s error: %v", data.OutFilePath, err)
+	}
+	defer file.Close()
+
+	fileWriter := bufio.NewWriter(file)
+	if err := png.Encode(fileWriter, img); err != nil {
+		return fmt.Errorf("png encode data to file error: %v", err)
+	}
+
+	if err := fileWriter.Flush(); err != nil {
+		return fmt.Errorf("flush data to out file error: %v", err)
+	}
+
+	return nil
+}
+
 func convert3857To4326(x, y float64) (lat, lon float64) {
-	lon = x * 180.0 / 20037508.34
-	lat = math.Atan(math.Exp(y*math.Pi/20037508.34))*360.0/math.Pi - 90.0
+	lon = x * 180.0 / MaxValue
+	lat = math.Atan(math.Exp(y*math.Pi/MaxValue))*360.0/math.Pi - 90.0
 
 	return
+}
+
+func convert4326To3857(lat, lon float64) (x, y float64) {
+	x = lon * MaxValue / 180
+	y = math.Log(math.Tan((90+lat)*math.Pi/360)) / (math.Pi / 180)
+	y = y * MaxValue / 180
+
+	return
+}
+
+func DrawIrregular3857(data *IrregularData, colorFunc ColorFunc) error {
+	if err := data.check(); err != nil {
+		return err
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, 2049, 2049))
+	for index := 0; index < len(data.LatList); index++ {
+		lat := data.LatList[index]
+		lon := data.LonList[index]
+		x, y := convert4326To3857(lat, lon)
+
+		xIndex := int((x - MinValue) / StepValue)
+		yIndex := 2049 - int((y-MinValue)/StepValue)
+
+		value := data.ValueList[index]
+		colorFunc(img, xIndex, yIndex, value)
+	}
+
+	file, err := os.Create(data.OutFilePath)
+	if err != nil {
+		return fmt.Errorf("create out file: %s error: %v", data.OutFilePath, err)
+	}
+	defer file.Close()
+
+	fileWriter := bufio.NewWriter(file)
+	if err := png.Encode(fileWriter, img); err != nil {
+		return fmt.Errorf("png encode data to file error: %v", err)
+	}
+
+	if err := fileWriter.Flush(); err != nil {
+		return fmt.Errorf("flush data to out file error: %v", err)
+	}
+
+	return nil
 }
